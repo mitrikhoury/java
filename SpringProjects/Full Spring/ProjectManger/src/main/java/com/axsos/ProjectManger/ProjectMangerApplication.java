5d99d7521a7da2be231595ec2966a91887d55161
mitrikhoury <mitkhoury@gmail.com>
2024-08-31 23:52:00 +0300

    Project Manger

---
+++ SpringProjects/Full Spring/ProjectManger/src/main/java/com/axsos/ProjectManger/ProjectMangerApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.ProjectManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMangerApplication.class, args);
+	}
+
+}
